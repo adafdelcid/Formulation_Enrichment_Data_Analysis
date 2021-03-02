4a3d8caff86d05c1b0261d10ea36408ccfe7871b
Ada Del Cid <ada.delcid10@gmail.com>
2021-03-02 12:56:24 -0500

    Update Enrichment_interface.py
    Turned into OOP and ran Pylint

--- Enrichment_interface.py
+++ Enrichment_interface.py
@@ -1,183 +1,238 @@
 '''
-Author: Ada Del Cid 
+Author: Ada Del Cid
 GitHub: @adafdelcid
 Jan.2021
 
 Enrichment_interface: Graphical user interface (GUI) for CSV2Excel_Functionalized.py
 '''
-from tkinter import *
-from tkinter import filedialog
-import os.path
+from tkinter import filedialog, Tk, StringVar, Label, Button, Entry, OptionMenu
 from os import path
+import sys
+
 import CSV2Excel_Functionalized
 
-root = Tk()
-root.geometry("600x500")
-root.title("Enrichment Analysis Tool")
+class MyGUI:
+	'''
+	GUI for enrichment analysis
+	'''
+	def __init__(self, master):
+		'''
+		Saves basic GUI buttons and data entries
+		'''
+		self.master = master
+		master.geometry("600x500")
+		master.title("Enrichment Analysis Tool")
 
-# string variables from user input
-fsp = StringVar() # Formulation Sheet file Path 
-ncp = StringVar() # Normalized Counts file Path
-sc = StringVar()  # list of Sorted Cells
-dfp = StringVar() # Destination Folder Path
-tbp = StringVar() # Top/Bottom Percent
-op = StringVar()  # Outliers Percentile
-org = StringVar()  # Organ
-ct = StringVar()  # Cell Type
+		# string variables from user input
+		self.fsp = StringVar() # Formulation Sheet file Path
+		self.ncp = StringVar() # Normalized Counts file Path
+		self.sc = StringVar()  # list of Sorted Cells
+		self.dfp = StringVar() # Destination Folder Path
+		self.tbp = StringVar() # Top/Bottom Percent
+		self.op = StringVar()  # Outliers Percentile
+		self.org = StringVar() # Organ
+		self.ct = StringVar()  # Cell Type
 
-def exit1():
-	exit()
+		Label(master, text="Enrichment Analysis", relief="solid", font=("arial", 16,\
+			"bold")).pack()
 
-def open_excel_file():
-	global fsp
-	fsp = filedialog.askopenfilename()
+		Label(master, text="Formulation Sheet File Path", font=("arial", 12,\
+			"bold")).place(x=20, y=50)
 
-def open_csv_file():
-	global ncp
-	ncp = filedialog.askopenfilename()
+		Button(master, text="Formulation sheet file", width=20, fg="green", font=("arial",\
+			16), command=self.open_excel_file).place(x=370, y=48)
 
-def enrichment_analysis():
+		Label(master, text="Normalized Counts CSV File Path", font=("arial", 12,\
+			"bold")).place(x=20, y=82)
+		Button(master, text="Normalized counts file", width=20, fg="green", font=("arial",\
+			16), command=self.open_csv_file).place(x=370, y=80)
 
-	var1 = fsp # Formulation Sheet file Path
-	var2 = ncp # Normalized Counts file Path
-	var3 = sc.get()  # list of Sorted Cells
-	var4 = dfp.get() # Destination Folder Path
-	var5 = tbp.get() # Top/Bottom Percent
-	var6 = op.get()  # Outliers Percentile
-	var7 = org.get()  # Organ
-	var8 = ct.get()  # Cell Type
+		Label(master, text="List of Sorted Cells (separate by commas)", font=("arial", 12,\
+			"bold")).place(x=20, y=114)
+		Entry(master, textvariable=self.sc).place(x=370, y=112)
 
-	var3 = string_to_list(var3)
-	var9 = get_cell_type(var7,var8)
+		Label(master, text="Destination Folder Path", font=("arial", 12, "bold")).place(\
+			x=20, y=146)
+		Entry(master, textvariable=self.dfp).place(x=370, y=144)
 
-	errors = False
+		Label(master, text="Top/Bottom Percent", font=("arial", 12, "bold")).place(x=20,\
+			y=178)
+		Entry(master, textvariable=self.tbp).place(x=370, y=176)
 
-	# check for errors with formulation sheet
-	if ".xlsx" in var1 and path_exists(var1):
-		color1 = "white"
-	else:
-		color1 = "red"
-		errors = True
-	
-	label9 = Label(root,text = "Invalid formulation Sheet file path!",fg = color1, font = ("arial",12,"bold")).place(x = 20, y = 310)
+		Label(master, text="(OPTIONAL: Default = 99.9) Outliers Percentile", font=("arial",\
+			12, "bold")).place(x=20, y=212)
+		Entry(master, textvariable=self.op).place(x=370, y=210)
 
-	# check for errors with normalized counts csv file
-	if ".csv" in var2 and path_exists(var2):
-		color2 = "white"
-	else:
-		color2 = "red"
-		errors = True
+		Label(master, text="Analyze by Cell Type", font=("arial", 12, "bold")).place(x=20,\
+			y=244)
 
-	label10 = Label(root,text = "Invalid normalized counts file path!",fg = color2, font = ("arial",12,"bold")).place(x = 20, y = 330)
+		# drop down menu
+		organ = ["Liver", "Lung", "Spleen", "Heart", "Kidney", "Pancreas", "Marrow", "Muscle",\
+		"Brain", "Lymph Node", "Thymus"]
+		cell_type = ["Hepatocytes", "Endothelial", "Kupffer", "Other Immune", "Dendritic",\
+		"B cells", "T cells", "Macrophages", "Epithelial", "Hematopoetic Stem Cells", "Fibroblasts",\
+		"Satellite Cells", "Other"]
 
-	# check for erros with destination folder
-	if path_exists(var4):
-		color3 = "white"
-	else:
-		color3 = "red"
-		errors = True
+		org_droplist = OptionMenu(master, self.org, *organ)
+		self.org.set("Select Organ")
+		org_droplist.config(width=15)
+		org_droplist.place(x=200, y=242)
 
-	label11 = Label(root,text = "Invalid destination folder!",fg = color3, font = ("arial",12,"bold")).place(x = 20, y = 350)
+		ct_droplist = OptionMenu(master, self.ct, *cell_type)
+		self.ct.set("Select Cell Type")
+		ct_droplist.config(width=15)
+		ct_droplist.place(x=384, y=242)
 
-	# check for erros with top/bottom percent
-	try:
-		color4 = "white"
-		var5 = float(var5)
-	except:
-		color4 = "red"
-		errors = True
+		Button(master, text="ENTER", width=16, fg="blue", font=("arial", 16),\
+			command=self.enrichment_analysis).place(x=150, y=280)
+		Button(master, text="CANCEL", width=16, fg="blue", font=("arial", 16),\
+			command=exit1).place(x=300, y=280)
 
-	label12 = Label(root,text = "Invalid top/bottom percent, enter a value between 0.1-99.9!",fg = color4, font = ("arial",12,"bold")).place(x = 20, y = 370)
+	def open_excel_file(self):
+		'''
+		To open a file searcher and select a file
+		'''
+		self.fsp = filedialog.askopenfilename()
 
-	# check for errors with outliers percentile
-	if var6 != "":
-		try:
-			color5 = "white"
-			var6 = float(var6)
-		except:
-			color5 = "red"
+	def open_csv_file(self):
+		'''
+		To open a file searcher and csv file
+		'''
+		self.ncp = filedialog.askopenfilename()
+
+	def enrichment_analysis(self):
+		'''
+		Checks for any entry errors, returns list of errors or runs the enrichment analysis
+		'''
+
+		var1 = string_to_list(self.sc.get()) # list of cell types
+		var2 = get_cell_type(self.org.get(), self.ct.get()) # cell type to base analysis
+		var3 = self.dfp.get() # Destination Folder Path
+		var4 = self.tbp.get() # Top/Bottom Percent
+		var5 = self.op.get()  # Outliers Percentile
+
+		errors = False
+
+		# check for errors with formulation sheet
+		if ".xlsx" in self.fsp and path_exists(self.fsp):
+			color1 = "white"
+		else:
+			color1 = "red"
 			errors = True
-	else:
-		color5 = "white"
-		var6 = 99.9
 
-	label13 = Label(root,text = "Invalid outlier percentile, enter a value between 0.1-99.9 or leave blank!",fg = color5, font = ("arial",12,"bold")).place(x = 20, y = 390)
+		Label(self.master, text="Invalid formulation sheet file path!", fg=color1,\
+			font=("arial", 12, "bold")).place(x=20, y=310)
 
-	# check for errors with cell type to sort by
-	if var9 not in var3:
-		color6 = "red"
-		errors = True
-	else:
-		color6 = "white"
+		# check for errors with normalized counts csv file
+		if ".csv" in self.ncp and path_exists(self.ncp):
+			color2 = "white"
+		else:
+			color2 = "red"
+			errors = True
 
-	label13 = Label(root,text = "Invalid cell type to sort by, not in list of sorted cells",fg = color6, font = ("arial",12,"bold")).place(x = 20, y = 410)
+		Label(self.master, text="Invalid normalized counts file path!", fg=color2,\
+			font=("arial", 12, "bold")).place(x=20, y=330)
 
+		# check for erros with destination folder
+		if path_exists(var3):
+			color3 = "white"
+		else:
+			color3 = "red"
+			errors = True
 
-	if not errors:
-		CSV2Excel_Functionalized.run_enrichment_analysis(var4, var1, var2, var3, var5, var9, var6)
-		print("Enrichment analysis performed!")
-		exit()
-	else:
-		root.mainloop()
+		Label(self.master, text="Invalid destination folder!", fg=color3,\
+			font=("arial", 12, "bold")).place(x=20, y=350)
+
+		# check for erros with top/bottom percent
+		try:
+			color4 = "white"
+			var4 = float(var4)
+		except TypeError:
+			color4 = "red"
+			errors = True
+
+		Label(self.master,\
+			text="Invalid top/bottom percent, enter a value between 0.1-99.9!",\
+			fg=color4, font=("arial", 12, "bold")).place(x=20, y=370)
+
+		# check for errors with outliers percentile
+		if var5 != "":
+			try:
+				color5 = "white"
+				var5 = float(var5)
+			except TypeError:
+				color5 = "red"
+				errors = True
+		else:
+			color5 = "white"
+			var5 = 99.9
+
+		Label(self.master,\
+			text="Invalid outlier percentile, enter a value between 0.1-99.9 or leave blank!",\
+			fg=color5, font=("arial", 12, "bold")).place(x=20, y=390)
+
+		# check for errors with cell type to sort by
+		if var2 not in var1:
+			color6 = "red"
+			errors = True
+		else:
+			color6 = "white"
+
+		Label(self.master,\
+			text="Invalid cell type to sort by, not in list of sorted cells",\
+			fg=color6, font=("arial", 12, "bold")).place(x=20, y=410)
+
+		if not errors:
+			CSV2Excel_Functionalized.run_enrichment_analysis(var3, self.fsp, self.ncp, var1,\
+				var4, var2, var5)
+			print("Enrichment analysis performed!")
+			exit1()
+		else:
+			self.master.mainloop()
 
 def get_cell_type(organ, cell_type):
-	organ_dict = {"Liver" : "V", "Lung" : "L" , "Spleen" : "S", "Heart" : "H", "Kidney" : "K", "Pancreas" : "P", "Marrow" : "M", "Muscle" : "U", "Brain" : "B", "Lymph Node" : "N", "Thymus" : "T"}
-	cell_type_dict = {"Hepatocytes" : "H", "Endothelial" : "E", "Kupffer" : "K", "Other Immune" : "I", "Dendritic" : "D", "B cells" : "B", "T cells" : "T", "Macrophages" : "M", "Epithelial" : "EP", "Hematopoetic Stem Cells" : "HSC", "Fibroblasts" : "F", "Satellite Cells" : "SC", "Other" : "O"}
+	'''
+	Returns acronym of organ and cell type
+	'''
+
+	organ_dict = {"Liver":"V", "Lung":"L", "Spleen":"S", "Heart":"H", "Kidney":"K",\
+	"Pancreas":"P", "Marrow":"M", "Muscle":"U", "Brain":"B", "Lymph Node":"N",\
+	"Thymus":"T"}
+	cell_type_dict = {"Hepatocytes":"H", "Endothelial":"E", "Kupffer":"K", "Other Immune":"I",\
+	"Dendritic":"D", "B cells":"B", "T cells":"T", "Macrophages":"M", "Epithelial":"EP",\
+	"Hematopoetic Stem Cells":"HSC", "Fibroblasts":"F", "Satellite Cells":"SC", "Other":"O"}
 	return organ_dict[organ] + cell_type_dict[cell_type]
 
+def exit1():
+	'''
+	exit and close GUI
+	'''
+
+	sys.exit()
 
 def string_to_list(string1):
+	'''
+	Creates a list out of a string of items separated by commas
+	'''
+
 	string1 = remove_spaces(string1)
 	list1 = list(string1.split(","))
 	return list1
 
 def remove_spaces(string1):
-	return string1.replace(" ","")
+	'''
+		remove any unnecessary spaces
+	'''
+
+	return string1.replace(" ", "")
 
 def path_exists(path1):
+	'''
+	Checks if a directory path exists
+	'''
+
 	return path.exists(path1)
 
-#photo1 = PhotoImage(file = r"C:")
-
-label1 = Label(root,text = "Enrichment Analysis",  relief = "solid", font = ("arial",16,"bold")).pack()
-
-label2 = Label(root,text = "Formulation Sheet File Path", font = ("arial",12,"bold")).place(x = 20, y = 50)
-#entry2 = Entry(root, textvariable=fsp).place(x = 370, y = 48)
-entry2 = Button(root, text = "Formulation sheet file", width = 20, fg = "green", font = ("arial",16), command = open_excel_file).place(x = 370, y = 48)
-
-label3 = Label(root,text = "Normalized Counts CSV File Path", font = ("arial",12,"bold")).place(x = 20, y = 82)
-#entry3 = Entry(root, textvariable=ncp).place(x = 370, y = 80)
-entry2 = Button(root, text = "Normalized counts file", width = 20, fg = "green", font = ("arial",16), command = open_csv_file).place(x = 370, y = 80)
-
-label4 = Label(root,text = "List of Sorted Cells (separate by commas)", font = ("arial",12,"bold")).place(x = 20, y = 114)
-entry4 = Entry(root, textvariable=sc).place(x = 370, y = 112)
-
-label5 = Label(root,text = "Destination Folder Path", font = ("arial",12,"bold")).place(x = 20, y = 146)
-entry5 = Entry(root, textvariable=dfp).place(x = 370, y = 144)
-
-label6 = Label(root,text = "Top/Bottom Percent", font = ("arial",12,"bold")).place(x = 20, y = 178)
-entry6 = Entry(root, textvariable=tbp).place(x = 370, y = 176)
-
-label7 = Label(root,text = "(OPTIONAL: Default = 99.9) Outliers Percentile", font = ("arial",12,"bold")).place(x = 20, y = 212)
-entry7 = Entry(root, textvariable=op).place(x = 370, y = 210)
-
-label8 = Label(root,text = "Analyze by Cell Type", font = ("arial",12,"bold")).place(x = 20, y = 244)
-
-# drop down menu
-organ = ["Liver", "Lung", "Spleen", "Heart", "Kidney", "Pancreas", "Marrow", "Muscle", "Brain", "Lymph Node", "Thymus"]
-cell_type = ["Hepatocytes", "Endothelial", "Kupffer", "Other Immune", "Dendritic", "B cells", "T cells", "Macrophages", "Epithelial", "Hematopoetic Stem Cells", "Fibroblasts", "Satellite Cells", "Other"]
-
-droplist = OptionMenu(root, org, *organ)
-org.set("Select Organ")
-droplist.config(width = 15)
-droplist.place(x = 200, y = 242)
-
-droplist = OptionMenu(root, ct, *cell_type)
-ct.set("Select Cell Type")
-droplist.config(width = 15)
-droplist.place(x = 384, y = 242)
-
-b1 = Button(root, text = "ENTER", width = 16, fg = "blue", font = ("arial",16), command = enrichment_analysis).place(x = 150, y = 280)
-b1 = Button(root, text = "CANCEL", width = 16, fg = "blue", font = ("arial",16), command = exit1).place(x = 300, y = 280)
-
+root = Tk()
+my_gui = MyGUI(root)
 root.mainloop()
